756b4d1fda78fef2240e90e62428d59db0f31fe4
YarikN <47899636+YarikN@users.noreply.github.com>
2019-04-03 18:16:51 +0300

    Update zambretti.py

--- zambretti.py
+++ zambretti.py
@@ -1,90 +1,93 @@
 from datetime import datetime
+from pymongo import MongoClient
+import pymongo
+import redis
+import json
 
-def result(msg) :
+
+def result(msg):
     forecast_text = {
-        'A' : "Settled fine",
-        'B' : "Fine weather",
-        'C' : "Becoming fine",
-        'D' : "Fine, becoming less settled",
-        'E' : "Fine, possible showers",
-        'F' : "Fairly fine, improving",
-        'G' : "Fairly fine, possible showers early",
-        'H' : "Fairly fine, showery later",
-        'I' : "Showery early, improving",
-        'J' : "Changeable, mending",
-        'K' : "Fairly fine, showers likely",
-        'L' : "Rather unsettled clearing later",
-        'M' : "Unsettled, probably improving",
-        'N' : "Showery, bright intervals",
-        'O' : "Showery, becoming less settled",
-        'P' : "Changeable, some rain",
-        'Q' : "Unsettled, short fine intervals",
-        'R' : "Unsettled, rain later",
-        'S' : "Unsettled, some rain",
-        'T' : "Mostly very unsettled",
-        'U' : "Occasional rain, worsening",
-        'V' : "Rain at times, very unsettled",
-        'W' : "Rain at frequent intervals",
-        'X' : "Rain, very unsettled",
-        'Y' : "Stormy, may improve",
-        'Z' : "Stormy, much rain"
-        }
+        'A': "Settled fine",
+        'B': "Fine weather",
+        'C': "Becoming fine",
+        'D': "Fine, becoming less settled",
+        'E': "Fine, possible showers",
+        'F': "Fairly fine, improving",
+        'G': "Fairly fine, possible showers early",
+        'H': "Fairly fine, showery later",
+        'I': "Showery early, improving",
+        'J': "Changeable, mending",
+        'K': "Fairly fine, showers likely",
+        'L': "Rather unsettled clearing later",
+        'M': "Unsettled, probably improving",
+        'N': "Showery, bright intervals",
+        'O': "Showery, becoming less settled",
+        'P': "Changeable, some rain",
+        'Q': "Unsettled, short fine intervals",
+        'R': "Unsettled, rain later",
+        'S': "Unsettled, some rain",
+        'T': "Mostly very unsettled",
+        'U': "Occasional rain, worsening",
+        'V': "Rain at times, very unsettled",
+        'W': "Rain at frequent intervals",
+        'X': "Rain, very unsettled",
+        'Y': "Stormy, may improve",
+        'Z': "Stormy, much rain"
+    }
     return forecast_text[msg]
 
 
-def zamberetti (p):
+def zamberetti(p):
     z = round(22 * (p - 950) / 100)
     return z
 
 
 def change_press_wind(pressureGet, windGet):
     if 349.75 <= windGet < 360:
-        return pressureGet + 6
-    if 0 <= windGet < 12.25:
-        return pressureGet + 6
-    if 12.25 <= windGet < 34.75:
-        return pressureGet + 5
-    if 34.75 <= windGet < 57.25:
-        return pressureGet + 5
-    if 57.25 <= windGet < 79.75:
-        return pressureGet + 2
-    if 79.5 <= windGet < 102.25:
-        return pressureGet - 0.5
-    if 102.25 <= windGet < 124.75:
-        return pressureGet - 2
-    if 124.75 <= windGet < 147.25:
-        return pressureGet - 5
-    if 147.5 <= windGet < 169.75:
-        return pressureGet - 8.5
-    if 169.75 <= windGet < 192.25:
-        return pressureGet - 12
-    if 192.25 <= windGet < 214.75:
-        return pressureGet - 10
-    if 214.75 <= windGet < 237.25:
-        return pressureGet - 6
-    if 237.25 <= windGet < 259.75 :
-        return pressureGet - 4.5
-    if 259.75 <= windGet < 282.25:
-        return pressureGet - 3
-    if 282.25 <= windGet < 304.75:
-        return pressureGet - 0.5
-    if 304.75 <= windGet < 327.25:
-        return pressureGet + 1.5
-    if 327.25 <= windGet < 349.75:
-        return pressureGet + 3
+        pressureGet += 6
+    elif 0 <= windGet < 12.25:
+        pressureGet += 6
+    elif 12.25 <= windGet < 34.75:
+        pressureGet += 5
+    elif 34.75 <= windGet < 57.25:
+        pressureGet += 5
+    elif 57.25 <= windGet < 79.75:
+        pressureGet += 2
+    elif 79.5 <= windGet < 102.25:
+        pressureGet -= 0.5
+    elif 102.25 <= windGet < 124.75:
+        pressureGet -= 2
+    elif 124.75 <= windGet < 147.25:
+        pressureGet -= 5
+    elif 147.5 <= windGet < 169.75:
+        pressureGet -= 8.5
+    elif 169.75 <= windGet < 192.25:
+        pressureGet -= 12
+    elif 192.25 <= windGet < 214.75:
+        pressureGet -= 10
+    elif 214.75 <= windGet < 237.25:
+        pressureGet -= 6
+    elif 237.25 <= windGet < 259.75:
+        pressureGet -= 4.5
+    elif 259.75 <= windGet < 282.25:
+        pressureGet -= 3
+    elif 282.25 <= windGet < 304.75:
+        pressureGet -= 0.5
+    elif 304.75 <= windGet < 327.25:
+        pressureGet += 1.5
+    elif 327.25 <= windGet < 349.75:
+        pressureGet += 3
+    return pressureGet
 
 
-def trend_func(wetherDict):
-    plast = wetherDict[0]['main']['pressure']
-    preplast = wetherDict[1]['main']['pressure']
-    prepreplast = wetherDict[2]['main']['pressure']
-
+def trend_func(plast, preplast, prepreplast):
     if (plast - preplast >= 4) and (plast - prepreplast >= 4) and (preplast - prepreplast >= 4):
         return 'rise'
-    elif (plast - preplast <= -4) and (plast - prepreplast <= -4)and (preplast - prepreplast <= -4):
+    elif (plast - preplast <= -4) and (plast - prepreplast <= -4) and (preplast - prepreplast <= -4):
         return 'fall'
     else:
         return 'stab'
+
 
 def change_press_month(pressure, trend, month):
     if 3 < month < 9 and trend == 'rise':
@@ -99,10 +102,12 @@ def fall_press(zam):
              'U', 'R', 'O', 'H', 'D', 'B', 'B', 'B', 'A', 'A', 'A')
     return fallP[zam]
 
+
 def rise_press(zam):
     riseP = ('Z', 'Z', 'Z', 'Y', 'Y', 'T', 'Q', 'M', 'L', 'J', 'I',
              'G', 'F', 'C', 'B', 'B', 'A', 'A', 'A', 'A', 'A', 'A')
     return riseP[zam]
+
 
 def stab_press(zam):
     stabP = ('Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'X', 'X', 'W', 'S', 'P',
@@ -110,68 +115,41 @@ def stab_press(zam):
     return stabP[zam]
 
 
-def main():
-    a = [{"coord":{"lon":139,"lat":35},
-        "sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
-        "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-        "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-        "wind":{"speed":7.31,"deg":187.002},
-        "rain":{"3h":0},
-        "clouds":{"all":92},
-        "dt":1369824698,
-        "id":1851632,
-        "name":"Shuzenji",
-        "cod":200},
+def main(request):
+    frompubsub = request.get_json()
+    # take the city for a key to make a demand in DB
+    city = frompubsub['name']
+    client = MongoClient('mongodb://main_admin:abc123@35.241.132.65/mysinoptik', 27017)
+    db = client.mysinoptik
+    # take 3 last inputs in DB on key "city"
+    fromdb = list(db.weather.find({"name": city}, {"wind": 1, "main.pressure": 1, 'dt': 1}).sort('dt', pymongo.DESCENDING).limit(3))
+    pressure = frompubsub['pressure']
 
-    {"coord": {"lon": 139, "lat": 35},
-         "sys": {"country": "JP", "sunrise": 1369769524, "sunset": 1369821049},
-         "weather": [{"id": 804, "main": "clouds", "description": "overcast clouds", "icon": "04n"}],
-         "main": {"temp": 289.5, "humidity": 89, "pressure": 1023, "temp_min": 287.04, "temp_max": 292.04},
-         "wind": {"speed": 7.31, "deg": 187.002},
-         "rain": {"3h": 0},
-         "clouds": {"all": 92},
-         "dt": 1369824698,
-         "id": 1851632,
-         "name": "Shuzenji",
-         "cod": 200},
+    # geting the prelast and preprelast data if the topic2 data == last data in sinoptik
+    if frompubsub['dt'] == fromdb[0]['dt']:
+        preplast = fromdb[1]['main']['pressure']
+        prepreplast = fromdb[2]['main']['pressure']
+    else:
+        preplast = fromdb[0]['main']['pressure']
+        prepreplast = fromdb[1]['main']['pressure']
 
-    {"coord": {"lon": 139, "lat": 35},
-         "sys": {"country": "JP", "sunrise": 1369769524, "sunset": 1369821049},
-         "weather": [{"id": 804, "main": "clouds", "description": "overcast clouds", "icon": "04n"}],
-         "main": {"temp": 289.5, "humidity": 89, "pressure": 1023, "temp_min": 287.04, "temp_max": 292.04},
-         "wind": {"speed": 7.31, "deg": 187.002},
-         "rain": {"3h": 0},
-         "clouds": {"all": 92},
-         "dt": 1369824698,
-         "id": 1851632,
-         "name": "Shuzenji",
-         "cod": 200}]
-
-
-
-
-
-    pressure = a[0]['main']['pressure']
-    wind = a[0]['wind']['deg']
-    dt = datetime.fromtimestamp(a[0]['dt'])
+    wind = frompubsub['wind']['deg']
+    mongo_dt = frompubsub['dt']
+    dt = datetime.fromtimestamp(frompubsub['dt'])
     month = dt.month
 
     pressure = change_press_wind(pressure, wind)
-    print(pressure)
 
-    trend = trend_func(a)
-    print(trend)
+    trend = trend_func(pressure, preplast, prepreplast)
 
     pressure = change_press_month(pressure, trend, month)
-    print(pressure)
 
     zamber = zamberetti(pressure)
-    print("zamber = {0}".format(zamber))
 
     trendLetter = {'fall': fall_press, 'rise': rise_press, 'stab': stab_press}
 
     prognosis = result(trendLetter[trend](zamber))
-    print(prognosis)
-
-
-main()
+    
+    toredis = json.dumps({'prognosis': prognosis, 'dt': mongo_dt}).encode('UTF-8')
+    r = redis.StrictRedis(host='35.187.98.192', port=6379, db=1)
+    r.set(city, toredis)
